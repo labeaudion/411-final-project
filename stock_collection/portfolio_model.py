e9612e0c6eae8a9ded7f94b4e2ce47388f9b921e
Lily Beaudion <123118619+labeaudion@users.noreply.github.com>
2024-12-07 18:23:44 -0500

    Update portfolio_model.py

--- stock_collection/portfolio_model.py
+++ stock_collection/portfolio_model.py
@@ -1,1 +1,25 @@
+import logging
+from typing import List
+from stock_collection.models.stock_model import Stock
+from stock_collection.utils.logger import configure_logger
 
+logger = logging.getLogger(__name__)
+configure_logger(logger)
+
+class PortfolioModel:
+    """
+    A class to manage a portfolio of stocks.
+
+    Attributes:
+        stock_list (List[Stock]): The list of stocks in the portfolio.
+
+    """
+
+    def __init__(self):
+        """
+        Initializes the PortfolioModel with an empty porfolio.
+        """
+        self.stock_list: List[Stock] = []
+
+  
+    
