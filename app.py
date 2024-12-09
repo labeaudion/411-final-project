4c1c4307a7b880e908e2bd9abf4b556dfe1c9105
Lily Beaudion <123118619+labeaudion@users.noreply.github.com>
2024-12-08 22:26:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,85 @@
 from dotenv import load_dotenv
 from flask import Flask, jsonify, make_response, Response, request
+from werkzeug.exceptions import BadRequest, Unauthorized
+# from flask_cors import CORS
+
+from config import ProductionConfig
+from stock_model.db import db
+from stock_collection.models.portfolio_model import PortfolioModel
+from stock_collection.models.stock_model import Stock
+from stock_collection.models.mongo_session_model import login_user, logout_user
+from stock_collection.models.user_model import Users
+
+# Load environment variables from .env file
+load_dotenv()
+
+def create_app(config_class=ProductionConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)  # Initialize db with app
+    with app.app_context():
+        db.create_all()  # Recreate all tables
+
+    portfolio_model = PortfolioModel()
+
+    ####################################################
+    #
+    # Healthchecks
+    #
+    ####################################################
+
+
+    @app.route('/api/health', methods=['GET'])
+    def healthcheck() -> Response:
+        """
+        Health check route to verify the service is running.
+
+        Returns:
+            JSON response indicating the health status of the service.
+        """
+        app.logger.info('Health check')
+        return make_response(jsonify({'status': 'healthy'}), 200)
+
+    ##########################################################
+    #
+    # User management
+    #
+    ##########################################################
+
+    @app.route('/api/create-user', methods=['POST'])
+    def create_user() -> Response:
+        """
+        Route to create a new user.
+
+        Expected JSON Input:
+            - username (str): The username for the new user.
+            - password (str): The password for the new user.
+
+        Returns:
+            JSON response indicating the success of user creation.
+        Raises:
+            400 error if input validation fails.
+            500 error if there is an issue adding the user to the database.
+        """
+        app.logger.info('Creating new user')
+        try:
+            # Get the JSON data from the request
+            data = request.get_json()
+
+            # Extract and validate required fields
+            username = data.get('username')
+            password = data.get('password')
+
+            if not username or not password:
+                return make_response(jsonify({'error': 'Invalid input, both username and password are required'}), 400)
+
+            # Call the User function to add the user to the database
+            app.logger.info('Adding user: %s', username)
+            Users.create_user(username, password)
+
+            app.logger.info("User added: %s", username)
+            return make_response(jsonify({'status': 'user added', 'username': username}), 201)
+        except Exception as e:
+            app.logger.error("Failed to add user: %s", str(e))
+            return make_response(jsonify({'error': str(e)}), 500)
