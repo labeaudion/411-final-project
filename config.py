3495203a27eca2678eb82e5b770bd7fd4cb3dc8e
Lily Beaudion <123118619+labeaudion@users.noreply.github.com>
2024-12-10 23:08:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import os
 class ProductionConfig():
     """Production configuration."""
     DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = True  # This would almost universally be false in a Flask app
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # This would almost universally be false in a Flask app
                                            # But we are doing unnecessarily complicated Redis
                                            # write-throughs
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "DATABASE_URL=sqlite:////app/db/app.db")  # Production database URI from environment
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(basedir, 'db', 'app.db')}")   # Production database URI from environment
 
 class TestConfig():
     """Testing configuration."""
